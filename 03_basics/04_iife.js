82107479f6f35e179ec96845b00063079ce7690f
mayankvmm <mayankfob@gmail.com>
2024-10-02 02:00:28 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,9 @@
+// Immediately Invoked Function Expressions )(IIFE)
+
+(function iife() {
+    console.log(`DB connected`);
+}) (); // Semi colon is must multiple iife execution
+
+( (arg) => {
+    console.log(`2nd DB connected ${arg}`);
+} ) ("Horray!")
